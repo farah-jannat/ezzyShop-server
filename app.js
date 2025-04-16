7c56da3e376c17ccb0c383ee6d0e73ace2e746bf
“farah-jannat” <farahjannatmithila@gmail.com>
2025-04-16 09:17:39 +0600

    Run server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const userrouter = require('./routes/userRouter.js');
+const connectdb = require('./db/connection.js')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
+const port = 8000;
+const database="mongodb+srv://farahjannatmithila:Yj9RBZesHnDHhYbj@cluster0.ngeelqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+
+app.use(bodyParser.json());
+app.use(cors());
+connectdb(database);
+app.use('/api/user', userrouter);
+
+app.listen(port,()=>{
+    console.log(`server is ruuning at ${port}`)
+})
