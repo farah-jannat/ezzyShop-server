f98eaf09549eecd26210da47aeddc54045015b23
“farah-jannat” <farahjannatmithila@gmail.com>
2025-04-18 19:51:29 +0600

    feat : add updatecategory api router

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,6 +6,7 @@ const upload = require("../middlewares/image-uploader.js");
 const categorylistLevelOne = require("../controllers/productCategory/categorylist_level_one.js");
 const categorySingle = require("../controllers/productCategory/categorySingle.js");
 const deleteCategory = require("../controllers/productCategory/deleteCategory.js");
+const updatedCategory = require("../controllers/productCategory/updateCategory.js");
 
 routercate.post(
   "/",
@@ -16,4 +17,9 @@ routercate.get("/", categorylist);
 routercate.get("/levelone", categorylistLevelOne);
 routercate.get("/:id", categorySingle);
 routercate.delete("/:id", deleteCategory);
+routercate.patch(
+  "/:id",
+  upload.fields([{ name: "category_image", maxCount: 1 }]),
+  updatedCategory
+);
 module.exports = routercate;
