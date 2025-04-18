019b4a69673f08d402b84c159ab9433fae9016f6
“farah-jannat” <farahjannatmithila@gmail.com>
2025-04-18 17:40:32 +0600

    feat : add categorysingle api router

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -4,6 +4,7 @@ const createCategory = require("../controllers/productCategory/createCategory.js
 const categorylist = require("../controllers/productCategory/categorylist.js");
 const upload = require("../middlewares/image-uploader.js");
 const categorylistLevelOne = require("../controllers/productCategory/categorylist_level_one.js");
+const categorySingle = require("../controllers/productCategory/categorySingle.js");
 
 routercate.post(
   "/",
@@ -12,4 +13,5 @@ routercate.post(
 );
 routercate.get("/", categorylist);
 routercate.get("/levelone", categorylistLevelOne);
+routercate.get("/:id", categorySingle);
 module.exports = routercate;
