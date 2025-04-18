6e62b32a0244c7f3388a8c6fabe41d193144fdcc
“farah-jannat” <farahjannatmithila@gmail.com>
2025-04-18 17:57:40 +0600

    feat: add deletecategory api router

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -5,6 +5,7 @@ const categorylist = require("../controllers/productCategory/categorylist.js");
 const upload = require("../middlewares/image-uploader.js");
 const categorylistLevelOne = require("../controllers/productCategory/categorylist_level_one.js");
 const categorySingle = require("../controllers/productCategory/categorySingle.js");
+const deleteCategory = require("../controllers/productCategory/deleteCategory.js");
 
 routercate.post(
   "/",
@@ -14,4 +15,5 @@ routercate.post(
 routercate.get("/", categorylist);
 routercate.get("/levelone", categorylistLevelOne);
 routercate.get("/:id", categorySingle);
+routercate.delete("/:id", deleteCategory);
 module.exports = routercate;
