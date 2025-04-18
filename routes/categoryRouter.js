fc14bb370d244108b13819ca068ddb1a1cc4f7e5
“farah-jannat” <farahjannatmithila@gmail.com>
2025-04-18 14:04:05 +0600

    feat : add all and perent category api router

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const routercate = express.Router();
 const createCategory = require("../controllers/productCategory/createCategory.js");
+const categorylist = require("../controllers/productCategory/categorylist.js");
 const upload = require("../middlewares/image-uploader.js");
+const categorylistLevelOne = require("../controllers/productCategory/categorylist_level_one.js");
 
 routercate.post(
   "/",
   upload.fields([{ name: "category_image", maxCount: 1 }]),
   createCategory
 );
+routercate.get("/", categorylist);
+routercate.get("/levelone", categorylistLevelOne);
 module.exports = routercate;
