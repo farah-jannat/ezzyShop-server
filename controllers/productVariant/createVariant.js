20e14cb0c61bb5dc939eb57a63bd71790c6d4e28
“farah-jannat” <farahjannatmithila@gmail.com>
2025-04-20 20:08:36 +0600

    feat : add createProductVarient functionality

---
+++ controllers/productVariant/createVariant.js
@@ -0,0 +1,66 @@
+const Variant = require("../../models/product_variant.js");
+const slugify = require("slugify");
+
+const createVariant = async (req, res) => {
+  try {
+    const {
+      product_id,
+      product_name,
+      product_url,
+      meta_title,
+      meta_keywords,
+      meta_description,
+      featuredproduct,
+      trendingproduct,
+      newarrivedproduct,
+      editor,
+      parent_category,
+      child_category,
+      sort_description,
+      weight_type,
+      weight,
+      stock,
+      mrp_price,
+      selling_price,
+      status,
+      color,
+      brand,
+      size,
+    } = req.body;
+
+    const insertvariant = new Variant({
+      product_id,
+      product_name,
+      sort_description,
+      product_url: slugify(product_url),
+      product_image1: req.files.product_image1[0].filename,
+      product_image2: req.files.product_image2[0].filename,
+      product_image3: req.files.product_image3[0].filename,
+      product_image4: req.files.product_image4[0].filename,
+      description: editor,
+      size,
+      color,
+      brand,
+      meta_title,
+      newarrivedproduct,
+      trendingproduct,
+      featuredproduct,
+      parent_category,
+      child_category,
+      meta_keywords,
+      meta_description,
+      weight_type,
+      selling_price,
+      mrp_price,
+      stock,
+      status,
+      weight,
+    });
+    const response = await insertvariant.save();
+    res.send({ status: "successfully", data: response });
+  } catch (err) {
+    console.log(`  here is errror ${err}`);
+    res.send({ status: "faild", errors: err });
+  }
+};
+module.exports = createVariant;
