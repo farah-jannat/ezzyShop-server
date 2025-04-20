370c463019d1ca3c6db3e5a82171b1ae6b6e9723
“farah-jannat” <farahjannatmithila@gmail.com>
2025-04-20 20:44:30 +0600

    feat : add updateProductVariant api route

--- routes/variantRouter.js
+++ routes/variantRouter.js
@@ -4,6 +4,7 @@ const createVariant = require("../controllers/productVariant/createVariant.js");
 const productVariantlist = require("../controllers/productVariant/productvariantlist.js");
 const deleteVariant = require("../controllers/productVariant/deleteVariant.js");
 const singleProductVarient = require("../controllers/productVariant/singleProductVariant.js");
+const updateProductVariant = require("../controllers/productVariant/updateProductVariant.js");
 
 router.post(
   "/",
@@ -18,5 +19,15 @@ router.post(
 router.get("/id/:id", productVariantlist);
 router.delete("/:id", deleteVariant);
 router.get("/:id", singleProductVarient);
+router.patch(
+  "/:id",
+  upload.fields([
+    { name: "product_image1", maxCount: 1 },
+    { name: "product_image2", maxCount: 1 },
+    { name: "product_image3", maxCount: 1 },
+    { name: "product_image4", maxCount: 1 },
+  ]),
+  updateProductVariant
+);
 
 module.exports = router;
