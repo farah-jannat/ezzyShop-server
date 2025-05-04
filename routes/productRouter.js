9f5dcec1fe31d464d1e39638b3bd8cba602875ed
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-04 12:06:32 +0600

    feat : add searchItem api router

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -6,6 +6,8 @@ const singleProduct = require("../controllers/product/singleProduct");
 const deleteProduct = require("../controllers/product/deleteProduct");
 const updateproduct = require("../controllers/product/updateProduct");
 const frontend_productlist_bycategory = require("../controllers/product/frontend/frontend_productlist_bycategory.js");
+const searchItem = require("../controllers/product/frontend/frontend_search.js");
+
 const router = express.Router();
 
 router.get("/", productlist);
@@ -31,6 +33,7 @@ router.patch(
   ]),
   updateproduct
 );
+router.get("/search/:name", searchItem);
 router.get("/product-by-category/:id", frontend_productlist_bycategory);
 
 module.exports = router;
