6c6e28730351c2c534779acc7b07923e5fed4250
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-04 18:23:27 +0600

    feat : add product to wishlist

---
+++ controllers/wishlist/addtowishlist.js
@@ -0,0 +1,24 @@
+const wishlist = require("../../models/wishlist.js");
+
+const addtowishlist = async (req, res) => {
+  try {
+    const { product_name, product_id, product_variant_id, item_or_variant } =
+      req.body;
+    const user_id = req.user.id;
+    const addproduct = new wishlist({
+      product_name,
+      product_id,
+      user_id,
+      product_variant_id,
+      item_or_variant,
+    });
+
+    const response = await addproduct.save();
+    res.send({ status: "successfully", data: response });
+  } catch (err) {
+    console.log(`Here is error: ${err}`);
+    res.send({ status: "failed", errors: err });
+  }
+};
+
+module.exports = addtowishlist;
