2fae3181a59f6bfdfeb241ab7cf4fa59b95dbef4
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-04 18:25:33 +0600

    feat : add api router for addtowishlist and wishlist_list

---
+++ routes/wishlistRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const authenticateToken = require("../middlewares/varifytoken.js");
+const addtowishlist = require("../controllers/wishlist/addtowishlist.js");
+const wishlist_list = require("../controllers/wishlist/wishlist_list.js");
+const router = express.Router();
+
+router.post("/", authenticateToken, addtowishlist);
+router.get("/", wishlist_list);
