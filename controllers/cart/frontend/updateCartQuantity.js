3d91e94136309063a91d064213b796308e807284
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-05 19:13:12 +0600

    feat : add update cart quntity functionality

---
+++ controllers/cart/frontend/updateCartQuantity.js
@@ -0,0 +1,28 @@
+const cart = require("../../../models/cart.js");
+
+const updateCartQuantity = async (req, res) => {
+  try {
+    const { cartItemId, newQuantity } = req.body;
+    const user_id = req.user.id;
+
+    const existingCartItem = await cart.findOne({
+      _id: cartItemId,
+      user_id,
+    });
+
+    if (existingCartItem) {
+      existingCartItem.product_qty = newQuantity;
+      await existingCartItem.save();
+      res.send({ status: "successfully", data: existingCartItem });
+    } else {
+      res
+        .status(404)
+        .send({ status: "failed", message: "Cart item not found" });
+    }
+  } catch (err) {
+    console.log(`Here is error: ${err}`);
+    res.status(500).send({ status: "failed", errors: err.message });
+  }
+};
+
+module.exports = updateCartQuantity;
