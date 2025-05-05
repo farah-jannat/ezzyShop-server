9fb2933699f7798f62f12bf33a23bcfbbd29387d
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-05 11:34:50 +0600

    feat : add frontend_singleproduct api route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,11 +2,13 @@ const express = require("express");
 const productlist = require("../controllers/product/productlist");
 const createProduct = require("../controllers/product/createProduct");
 const upload = require("../middlewares/image-uploader");
+const checkuser = require("../middlewares/checkuser.js");
 const singleProduct = require("../controllers/product/singleProduct");
 const deleteProduct = require("../controllers/product/deleteProduct");
 const updateproduct = require("../controllers/product/updateProduct");
 const frontend_productlist_bycategory = require("../controllers/product/frontend/frontend_productlist_bycategory.js");
 const searchItem = require("../controllers/product/frontend/frontend_search.js");
+const frontend_singleproduct = require("../controllers/product/frontend/frontend_singleproduct.js");
 
 const router = express.Router();
 
@@ -35,5 +37,6 @@ router.patch(
 );
 router.get("/search/:name", searchItem);
 router.get("/product-by-category/:id", frontend_productlist_bycategory);
+router.get("/product-detail/:id", checkuser, frontend_singleproduct);
 
 module.exports = router;
