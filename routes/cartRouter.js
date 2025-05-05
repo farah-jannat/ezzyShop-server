ab7875a45eb90de26a2ee1a7a5837c9ceac039db
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-05 19:13:42 +0600

    feat : add updateCartQuantity api route

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -5,11 +5,13 @@ const addtocartdelete = require("../controllers/cart/addtocartdelete.js");
 const cartlist = require("../controllers/cart/cartlist.js");
 const cartcount = require("../controllers/cart/frontend/cartcount.js");
 const checkuser = require("../middlewares/checkuser.js");
+const updateCartQuantity = require("../controllers/cart/frontend/updateCartQuantity.js");
 const router = express.Router();
 
 router.post("/", authenticateToken, addtocart);
 router.delete("/:cart_id", addtocartdelete);
 router.get("/", cartlist);
 router.get("/cartcount", checkuser, cartcount);
+router.post("/updateqty", authenticateToken, updateCartQuantity);
 
 module.exports = router;
