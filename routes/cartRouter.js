f2bbbc7e2ce438409df4fb11cbba73aaa5167eec
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-05 15:17:33 +0600

    feat : add cartlist api router

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -2,9 +2,11 @@ const express = require("express");
 const addtocart = require("../controllers/cart/addtocart.js");
 const authenticateToken = require("../middlewares/varifytoken.js");
 const addtocartdelete = require("../controllers/cart/addtocartdelete.js");
+const cartlist = require("../controllers/cart/cartlist.js")
 const router = express.Router();
 
 router.post("/", authenticateToken, addtocart);
 router.delete("/:cart_id", addtocartdelete);
+router.get("/", cartlist);
 
 module.exports = router;
