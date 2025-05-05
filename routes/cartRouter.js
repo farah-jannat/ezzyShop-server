ec1ab426dd3bc37c9285bb1c40867b856897878f
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-05 19:01:37 +0600

    feat : add cartcount api route

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -2,11 +2,14 @@ const express = require("express");
 const addtocart = require("../controllers/cart/addtocart.js");
 const authenticateToken = require("../middlewares/varifytoken.js");
 const addtocartdelete = require("../controllers/cart/addtocartdelete.js");
-const cartlist = require("../controllers/cart/cartlist.js")
+const cartlist = require("../controllers/cart/cartlist.js");
+const cartcount = require("../controllers/cart/frontend/cartcount.js");
+const checkuser = require("../middlewares/checkuser.js");
 const router = express.Router();
 
 router.post("/", authenticateToken, addtocart);
 router.delete("/:cart_id", addtocartdelete);
 router.get("/", cartlist);
+router.get("/cartcount", checkuser, cartcount);
 
 module.exports = router;
