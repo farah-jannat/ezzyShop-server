04e03bf0f4c88c6731e301b1ec524fe676c1131a
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-07 17:49:02 +0600

    feat : disply all orders

---
+++ controllers/order/orderlist.js
@@ -0,0 +1,15 @@
+const order = require("../../models/order.js");
+const orderlist = async (req, res) => {
+  try {
+    const orderlisting = await order
+      .find()
+      .populate("user_id", "first_name last_name email mobile")
+      .sort({ createdAt: -1 });
+    res.send({ status: "sucessfully", data: orderlisting });
+  } catch (err) {
+    console.log(`  here is errror ${err}`);
+    res.send({ status: "faild", errors: err });
+  }
+};
+
+module.exports = orderlist;
