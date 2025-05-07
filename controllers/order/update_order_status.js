0c031ff6e30831de6792f3647aefd0810da5e54f
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-07 17:49:30 +0600

    feat : update order status

---
+++ controllers/order/update_order_status.js
@@ -0,0 +1,23 @@
+const order = require("../../models/order.js");
+
+const updateorderstatus = async (req, res) => {
+  const orderid = req.params.id;
+  try {
+    const updatedOrder = await order.findByIdAndUpdate(orderid, req.body, {
+      new: true,
+    });
+
+    if (!updatedOrder) {
+      return res
+        .status(404)
+        .json({ status: "failed", message: "User not found" });
+    }
+
+    res.json({ status: "successfully update", data: updatedOrder });
+  } catch (err) {
+    console.error(`Error: ${err}`);
+    res.status(500).json({ status: "failed", errors: err.message });
+  }
+};
+
+module.exports = updateorderstatus;
