fc397d3a9a0c5215e68bace8a32a78b91375fd1c
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-07 17:54:23 +0600

    feat : add orderlist api route

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const createorder = require("../controllers/order/create_order.js");
 const authenticateToken = require("../middlewares/varifytoken.js");
+const orderlist = require("../controllers/order/orderlist.js");
 const router = express.Router();
 
 router.post("/", authenticateToken, createorder);
+router.get("/", orderlist);
