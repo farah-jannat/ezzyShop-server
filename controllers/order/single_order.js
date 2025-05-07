0c1cad274b44939dfc5c6937321729df0f372236
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-07 17:49:15 +0600

    feat : show single order

---
+++ controllers/order/single_order.js
@@ -0,0 +1,27 @@
+const order = require("../../models/order.js");
+const cart = require("../../models/cart.js");
+
+const singleorder = async (req, res) => {
+  try {
+    const data = await order.findById(req.params.id);
+
+    if (!data) {
+      return res.status(404).send({ error: "product not found" });
+    }
+    const existingCartItem = await cart
+      .find({ orderid: data.orderid })
+      .populate(
+        "product_variant_id",
+        "product_name product_image1 description selling_price mrp_price weight weighttype"
+      )
+      .populate(
+        "product_id",
+        "product_name product_image1 description selling_price mrp_price weight weighttype"
+      );
+    res.send({ status: "successfully", data, existingCartItem });
+  } catch (err) {
+    res.status(500).send({ error: "An error occurred while fetching data" });
+  }
+};
+
+module.exports = singleorder;
