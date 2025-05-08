66a0a433e6af3b0e1a82ba68cfaa0669beded14d
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-08 10:37:34 +0600

    feat : add update_order_status api route

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -3,6 +3,7 @@ const createorder = require("../controllers/order/create_order.js");
 const authenticateToken = require("../middlewares/varifytoken.js");
 const orderlist = require("../controllers/order/orderlist.js");
 const orderlistbyuser = require("../controllers/order/orderlist_by_user.js");
+const updateorderstatus = require("../controllers/order/update_order_status.js");
 const singleorder = require("../controllers/order/single_order");
 const router = express.Router();
 
@@ -10,3 +11,4 @@ router.post("/", authenticateToken, createorder);
 router.get("/", orderlist);
 router.get("/orderbyuser", authenticateToken, orderlistbyuser);
 router.get("/:id", singleorder);
+router.post("/changestatus/:id", updateorderstatus);
