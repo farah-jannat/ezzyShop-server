fcc4fd781ed160591b47ddf2b04be8306eb4e210
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-08 10:33:52 +0600

    feat : add singleorder api route

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -3,8 +3,10 @@ const createorder = require("../controllers/order/create_order.js");
 const authenticateToken = require("../middlewares/varifytoken.js");
 const orderlist = require("../controllers/order/orderlist.js");
 const orderlistbyuser = require("../controllers/order/orderlist_by_user.js");
+const singleorder = require("../controllers/order/single_order");
 const router = express.Router();
 
 router.post("/", authenticateToken, createorder);
 router.get("/", orderlist);
 router.get("/orderbyuser", authenticateToken, orderlistbyuser);
+router.get("/:id", singleorder);
